6db77b7ae42213d42f3bf91b23e7f399cdbb3776
Michael Eller <krabby127@gmail.com>
2016-01-23 16:59:30 -0700

    Improved speed by pre-calculating some variables

--- lqueval-BSmag-c14eb15/BSmag Core/BSmag_get_B.m
+++ lqueval-BSmag-c14eb15/BSmag Core/BSmag_get_B.m
@@ -40,7 +40,7 @@ for nF = 1:BSmag.Nfilament % Loop on each filament
     I = BSmag.filament(nF).I;
     
     % Discretization of Gamma
-%         x_P = []; y_P = []; z_P = [];
+    %         x_P = []; y_P = []; z_P = [];
     N = size(Gamma,1)-1; % Number of points defining Gamma
     
     for i = 1:N % Loop on the segments defining gamma
@@ -53,31 +53,55 @@ for nF = 1:BSmag.Nfilament % Loop on each filament
     DBx=zeros(1,size(BSmag.filament(BSmag.Nfilament).Gamma,1)-2);
     DBy=zeros(1,size(BSmag.filament(BSmag.Nfilament).Gamma,1)-2);
     DBz=zeros(1,size(BSmag.filament(BSmag.Nfilament).Gamma,1)-2);
+    lastSumX = 0;
+    lastSumY = 0;
+    lastSumZ = 0;
+    diffX=diff(x_P);
+    diffY=diff(y_P);
+    diffZ=diff(z_P);
     % Add contribution of each source point P on each field point M (where we want to calculate the field)
-    %Approximately 188,218,800 calculations
+    % Total
+    % callTotal = size(X,1)*size(X,2)*size(X,3)*(size(Gamma,1)-2);
+    count=0;
     for m = 1:size(X,1);
         for n = 1:size(X,2);
             for p = 1:size(X,3);
                 
                 % M is the field point
+                % This saves time on indexing
                 x_M = X(m,n,p);
                 y_M = Y(m,n,p);
                 z_M = Z(m,n,p);
                 
                 % Loop on each discretized segment of Gamma PkPk+1
                 for k = 1:length(x_P)-1
+                    % This is the most intensive call
+                    % Called size(X,1)*size(X,2)*size(X,3)*(size(Gamma,1)-2) times
+                    %
                     PkM3 = (sqrt((x_M-x_P(k))^2 + (y_M-y_P(k))^2 + (z_M-z_P(k))^2))^3;
-                    DBx(k) = ((y_P(k+1)-y_P(k))*(z_M-z_P(k))-(z_P(k+1)-z_P(k))*(y_M-y_P(k)))/PkM3;
-                    DBy(k) = ((z_P(k+1)-z_P(k))*(x_M-x_P(k))-(x_P(k+1)-x_P(k))*(z_M-z_P(k)))/PkM3;
-                    DBz(k) = ((x_P(k+1)-x_P(k))*(y_M-y_P(k))-(y_P(k+1)-y_P(k))*(x_M-x_P(k)))/PkM3;
+                    % DBx(k) = ((y_P(k+1)-y_P(k))*(z_M-z_P(k))-(z_P(k+1)-z_P(k))*(y_M-y_P(k)))/PkM3;
+                    % DBy(k) = ((z_P(k+1)-z_P(k))*(x_M-x_P(k))-(x_P(k+1)-x_P(k))*(z_M-z_P(k)))/PkM3;
+                    % DBz(k) = ((x_P(k+1)-x_P(k))*(y_M-y_P(k))-(y_P(k+1)-y_P(k))*(x_M-x_P(k)))/PkM3;
+                    DBx(k) = (diffY(k)*(z_M-z_P(k))-(diffZ(k))*(y_M-y_P(k)))/PkM3;
+                    DBy(k) = (diffZ(k)*(x_M-x_P(k))-(diffX(k))*(z_M-z_P(k)))/PkM3;
+                    DBz(k) = (diffX(k)*(y_M-y_P(k))-(diffY(k))*(x_M-x_P(k)))/PkM3;
+                    count=count+1;
                 end
+                
                 % Sum
-                BX(m,n,p) = BX(m,n,p) + mu0*I/4/pi*sum(DBx);
-                BY(m,n,p) = BY(m,n,p) + mu0*I/4/pi*sum(DBy);
-                BZ(m,n,p) = BZ(m,n,p) + mu0*I/4/pi*sum(DBz);
+                lastSumX = lastSumX + DBx(k);
+                lastSumY = lastSumY + DBy(k);
+                lastSumZ = lastSumZ + DBz(k);
+                % BX(m,n,p) = BX(m,n,p) + mu0*I/4/pi*sum(DBx);
+                % BY(m,n,p) = BY(m,n,p) + mu0*I/4/pi*sum(DBy);
+                % BZ(m,n,p) = BZ(m,n,p) + mu0*I/4/pi*sum(DBz);
+                
+                BX(m,n,p) = BX(m,n,p) + mu0*I/4/pi*lastSumX;
+                BY(m,n,p) = BY(m,n,p) + mu0*I/4/pi*lastSumY;
+                BZ(m,n,p) = BZ(m,n,p) + mu0*I/4/pi*lastSumZ;
                 
             end
         end
     end
-    
+    display(count);
 end
